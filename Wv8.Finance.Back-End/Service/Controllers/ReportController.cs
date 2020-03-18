8505cc1ed0d6b681609c458c467769f41b664b48
Wouter van Acht <wouter.van.acht@prodrive-technologies.com>
2020-03-18 17:43:41 +0100

    WIP: Start

---
+++ Wv8.Finance.Back-End/Service/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿namespace PersonalFinance.Service.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using PersonalFinance.Business.Account;
+    using PersonalFinance.Common.DataTransfer.Reports;
+
+    /// <summary>
+    /// Service endpoint for actions related to reports.
+    /// </summary>
+    [ApiController]
+    [Route("api/reports")]
+    public class ReportController : ControllerBase
+    {
+        // TODO: Replace with reports manager.
+        private readonly IAccountManager manager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReportController"/> class.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        public ReportController(IAccountManager manager)
+        {
+            this.manager = manager;
+        }
+
+        /// <summary>
+        /// Retrieves the report for the current date.
+        /// </summary>
+        /// <returns>The report.</returns>
+        [HttpGet("current-date")]
+        public CurrentDateReport GetCurrentDateReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
